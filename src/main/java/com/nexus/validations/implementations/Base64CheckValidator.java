ad6be5204e8c726c61e82e0bf614c34f0548059a
rafael.leite <rafael.pereira-terceiro@cea.com.br>
2024-02-01 13:39:49 -0300

    refactor: melhorias no codigo e separacao de responsabilidades

--- src/main/java/com/nexus/validations/implementations/Base64CheckValidator.java
+++ src/main/java/com/nexus/validations/implementations/Base64CheckValidator.java
@@ -11,19 +11,27 @@ import java.io.InputStream;
 import java.util.Base64;
 
 public class Base64CheckValidator implements ConstraintValidator<Base64Check, String> {
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        try {
-            byte[] decodedBytes = Base64.getDecoder().decode(value);
+        byte[] decodedBytes = decodeInBytes(value);
+        return validateImage(decodedBytes);
+    }
 
-            try (InputStream inputStream = new ByteArrayInputStream(decodedBytes)) {
-                BufferedImage image = ImageIO.read(inputStream);
-                if (image == null) {
-                    throw new IOException("Falha na validação: Não é uma imagem válida.");
-                }
-                return true;
+    private byte[] decodeInBytes(String object) {
+        return Base64.getDecoder().decode(object);
+    }
+
+    private boolean validateImage(byte[] bytesImage) {
+        try (InputStream stream = new ByteArrayInputStream(bytesImage)) {
+            BufferedImage image = ImageIO.read(stream);
+
+            if (image == null) {
+                throw new IOException("Falha na validação: Não é uma imagem válida.");
             }
-        } catch (IOException | IllegalArgumentException e) {
+
+            return true;
+        } catch (IOException e) {
             return false;
         }
     }
